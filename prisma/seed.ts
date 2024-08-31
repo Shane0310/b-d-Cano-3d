bbbd0744a681106663bcb346761260dad9f12e30
Shane0310 <Shanecano044@gmail.com>
2024-08-31 19:12:37 +0800

    added new file

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -7,6 +7,8 @@ const seed = async () => {
     data: [
       { title: 'title-1', content: 'dummy1' },
       { title: 'title-2', content: 'dummy2' },
+      { title: 'title-3', content: 'dummy3' },
+      { title: 'title-4', content: 'dummy4' },
     ],
   });
 };
